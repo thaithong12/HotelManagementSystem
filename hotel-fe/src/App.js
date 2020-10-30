c6cfa9ddf940c66c769f838f138615b088e751ed
thaithong12 <nguyenthaithong98@gmail.com>
2020-10-30 20:43:27 +0700

    Login Page FE

--- hotel-fe/src/App.js
+++ hotel-fe/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import login from "./login";
+import dashboard from "./dashboard";
 
 function App() {
   return (
     <div className="App">
-
+      <header className="App-header">
+        <BrowserRouter>
+          <Route exact path="/" component={login} />
+          <Route exact path="/dashboard" component={dashboard} />
+        </BrowserRouter>
+      </header>
     </div>
   );
 }
