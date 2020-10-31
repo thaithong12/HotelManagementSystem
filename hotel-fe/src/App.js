bf2086e283838f9009af095b019a76e749b61252
thqq21 <t.h.qq21@gmail.com>
2020-10-31 22:44:13 +0700

    1.0.0 Dashboard Admin

--- hotel-fe/src/App.js
+++ hotel-fe/src/App.js
@@ -1,12 +1,77 @@
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
 import './App.css';
+import Admin from './Admin';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    return (
+      <div className="App">
+        <Router>
+          <Page/>
+        </Router>
+        
+        
+      </div>
+    );
+  
 }
 
-export default App;
+
+
+function Page(){
+  return(
+    <div>
+      <header>
+        
+        <Link to="/Dashboard">Dashboard</Link>
+      </header>
+      <main>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/Dashboard" component={Dashboard}/>
+      </Switch> 
+      </main>
+      <footer>
+
+      </footer>
+       
+      
+    </div>
+  )
+}
+
+function Home(props) {
+  console.log(props.match);
+  return(
+    <div>
+      <h2>Home</h2>
+    </div>
+  )
+}
+function Dashboard() {
+  
+  return(
+    <Admin/>
+  )
+}
+function About() {
+  
+  return(
+    <div>
+      <h2>About</h2>
+    </div>
+  )
+}
+
+
+export default App ;
---
+++ hotel-fe/src/Components/Admin/SlideBar.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+export default function SlideBar() {
+    
+    return (
+        <aside className="menu-sidebar">
+        <div className="logo">
+            <Link to="/dashboard">
+            <img src="../images/logo1.png" alt="Admin" />
+            </Link>
+                
+            
+        </div>
+        <div className="menu-sidebar__content">
+            <nav className="navbar-sidebar">
+                <ul className="navbar__list">
+                    <li>
+                        <Link to="/dashboard">Dashboard</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard/rooms">Rooms</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard/categories">Categories</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard/bookings">Bookings</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard/promotions">Promotions</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard/services">Services</Link>
+                    </li>
+                        
+                        
+                </ul>
+            </nav>
+        </div>
+        </aside>
+    )
+    
+}
