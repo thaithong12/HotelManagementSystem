f2227ea72c37cafc03f971cef38a7b3b32d97d9c
thaithong12 <nguyenthaithong98@gmail.com>
2020-11-04 00:04:27 +0700

    Create Form login & register

--- hotel-fe/src/Components/User/Auth/Form/LoginForm.js
+++ hotel-fe/src/Components/User/Auth/Form/LoginForm.js
@@ -3,68 +3,72 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-
-import Box from "@material-ui/core/Box";
 import React from "react";
 import useStyles from "../Style";
 import {Link} from "react-router-dom";
-
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import Typography from "@material-ui/core/Typography";
 
 export default function LoginForm() {
   const classes = useStyles();
+
   return (
-    <form className={classes.form} noValidate>
-      <TextField
-        variant="outlined"
-        margin="normal"
-        required
-        fullWidth
-        id="email"
-        label="Email Address"
-        name="email"
-        autoComplete="email"
-        autoFocus
-      />
-      <TextField
-        variant="outlined"
-        margin="normal"
-        required
-        fullWidth
-        name="password"
-        label="Password"
-        type="password"
-        id="password"
-        autoComplete="current-password"
-      />
-      <FormControlLabel
-        control={<Checkbox value="remember" color="primary" />}
-        label="Remember me"
-      />
-      <Button
-        type="submit"
-        fullWidth
-        variant="contained"
-        color="primary"
-        className={classes.submit}
-      >
-        Sign In
-      </Button>
-      <Grid container>
-        <Grid item xs>
-        {/*  <Link href="#" variant="body2">*/}
-        {/*    Forgot password?*/}
-        {/*  </Link>*/}
-        {/*</Grid>*/}
-        {/*<Grid item>*/}
-        {/*  <Link href={'/register'} variant="body2">*/}
-        {/*    {"Don't have an account? Sign Up"}*/}
-        {/*  </Link>*/}
-        <Link to={'/register'} >Register</Link>
+    <>
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <form className={classes.form} noValidate>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+        >
+          Sign In
+        </Button>
+        <Grid container>
+          <Grid item xs>
+          {/*  <Link href="#" variant="body2">*/}
+          {/*    Forgot password?*/}
+          {/*  </Link>*/}
+          {/*</Grid>*/}
+          {/*<Grid item>*/}
+          {/*  <Link href={'/register'} variant="body2">*/}
+          {/*    {"Don't have an account? Sign Up"}*/}
+          {/*  </Link>*/}
+          <Link to={'/register'} >Register</Link>
+
+          </Grid>
         </Grid>
-      </Grid>
-      <Box mt={5}>
-        {/*<Copyright />*/}
-      </Box>
-    </form>
+      </form>
+    </>
   )
 }
--- hotel-fe/src/Components/User/Auth/Form/RegisterForm.js
+++ hotel-fe/src/Components/User/Auth/Form/RegisterForm.js
@@ -1,7 +1,105 @@
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import {Link} from "react-router-dom";
+import Box from "@material-ui/core/Box";
+import React from "react";
+import useStyles from "../Style";
+import Typography from "@material-ui/core/Typography";
 
 
 export default function RegisterForm() {
+  const classes = useStyles();
+
   return (
-    <h2>Day la Form Register</h2>
+    <>
+      <Typography component="h1" variant="h5">
+        Register
+      </Typography>
+      <form className={classes.form} noValidate>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="f_name"
+          label="Full Name"
+          type="text"
+          id="password"
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="f_name"
+          label="Phone Number"
+          type="text"
+          id="password"
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="re_password"
+          label="Re-Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+        >
+          Sign In
+        </Button>
+        <Grid container>
+          <Grid item xs>
+            {/*  <Link href="#" variant="body2">*/}
+            {/*    Forgot password?*/}
+            {/*  </Link>*/}
+            {/*</Grid>*/}
+            {/*<Grid item>*/}
+            {/*  <Link href={'/register'} variant="body2">*/}
+            {/*    {"Don't have an account? Sign Up"}*/}
+            {/*  </Link>*/}
+            <Link to={'/login'} >Login</Link>
+
+          </Grid>
+        </Grid>
+      </form>
+    </>
   )
 }
