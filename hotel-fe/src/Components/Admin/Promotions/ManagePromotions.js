51cc5fc94fbd2dd464700b8007fd07aadf679e69
thqq21 <t.h.qq21@gmail.com>
2020-11-23 23:32:05 +0700

    quan2

--- hotel-fe/src/Components/Admin/Promotions/ManagePromotions.js
+++ hotel-fe/src/Components/Admin/Promotions/ManagePromotions.js
@@ -261,7 +261,7 @@ export default function ManagePromotions() {
                     Yes
                   </Button>
                 </DialogActions>
-              </Dialog>
+            </Dialog>
               
                 
             </div>
