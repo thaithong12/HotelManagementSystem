b9ec25d9b1fa5126968a6c4dc6ea071d64f79142
duyen2631999 <duyen2631999@gmail.com>
2020-11-23 10:09:40 +0700

    Check for meeeee

---
+++ hotel-fe/src/Actions/roomCategoryAction.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+import {API_URL} from "../Constans/apiConstants";
+export const _getCategory = (categories) => ({
+    type: 'GET_CATEGORYs',
+    categories
+});
+
+export const _deleteCategory = (categories) => ({
+    type: 'DELETE_CATEGORY',
+    categories
+})
+
+export function getCategories() {
+    return (dispatch) => {
+        return axios.get(API_URL+'/categories')
+        .then(result => {
+            const categories = [];
+            result.data.response.data.forEach(item => {
+                categories.push(item);                
+            });
+        dispatch(_getCategory(categories));
+        });
+    };
+}
+
+export function deleteCategories(categories) {
+    return (dispatch) => {
+        return axios.delete(API_URL+'/categories',{data: categories})
+        .then(() => {
+            dispatch(_deleteCategory(categories));
+        })
+    }
+}
+
+export function addOrUpdateCategories(categories){
+    return (dispatch) => {
+        return axios.post(API_URL+'/categories', {data: categories})
+        .then(() => {
+            dispatch(getCategories());
+        });
+    }
+}
+
--- hotel-fe/src/Reducers/rootReducer.js
+++ hotel-fe/src/Reducers/rootReducer.js
@@ -1,12 +1,16 @@
 import userReducer from './userReducer'
 import {combineReducers} from "redux";
 import roomReducer from "./roomReducer";
+import serviceReducer from "./serviceReducer";
+import convenienceReducer from "./convenienceReducer";
+import roomCategoryReducer from "./roomCategoryReducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
   rooms: roomReducer,
   services: serviceReducer,
-  conveniences: convenienceReducer
+  conveniences: convenienceReducer,
+  categories: roomCategoryReducer
 })
 
 export default rootReducer;
